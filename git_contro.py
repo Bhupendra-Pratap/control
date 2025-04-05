898dc0e2c9639ab5bbe746bfeabd52b422d4f809
Bhupendra-Pratap <bhupendrasingh2092@gmail.com>
2025-04-05 11:05:25 +0530

    commit

--- git_contro.py
+++ git_contro.py
@@ -3,9 +3,9 @@ from random import randint
 
 # command used git commit --date="x days ago" -m "message"
 
-for i in range(1, 50):
+for i in range(1, 77):
 
-    for j in range(0, randint(0,3)):
+    for j in range(0, randint(0, 5)):
         d = str(i)+' days ago '
         with open('test.txt','a') as file:
             file.write(d)
